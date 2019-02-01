c780ca5bd66f0c7890283e94760e5427a5c5e1d2
Shirley Wang <shirleywang527@gmail.com>
2019-02-01 19:44:38 +0100

    add media queries

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-(function () {
-    $('.hamburger-menu').on('click', function() {
-      $(this).toggleClass('animate');
-      $('.bar').toggleClass('animate');
-    })
-  })();
+$(document).ready(function() {
+  $('.hamburger-menu').on('click', function() {
+    $(this).toggleClass('animate');
+    $('.bar').toggleClass('animate');
+  })
+
+  if($(window).width() < 768) {
+    console.log('less than 768');
+    $('.menu-list').hide();
+    //   toggle menu list
+    $('.hamburger-menu').click(function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $(this).toggleClass('open');
+      $('.menu-list').slideToggle(200);
+    });
+  }else {
+    console.log('greater than 768');
+  }
+
+});
