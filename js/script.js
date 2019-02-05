eec8079db49f1062b21ae48e1298dcf395cf1878
Shirley Wang <shirleywang527@gmail.com>
2019-02-05 10:41:34 +0100

    correct typo in index.html, add .hidden to .scrollTop & js -- scrolling 1/3 of the page the arrow btn shows

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-
+    // scroll-top arrow only appears after scrolling down half the page
+    $(window).scroll(function() {
+        if($(window).scrollTop() > $('body').height() / 3) {
+            $('.scrollTop').removeClass('hidden');
+        }else {
+            $('.scrollTop').addClass('hidden');
+        }
+    });
 });
