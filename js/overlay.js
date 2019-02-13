e39c88b4d87de4e48e1abff12e4425dd3b46ea1f
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2019-02-13 18:32:31 +0100

    ny Hire button (rad 180/index)

--- js/overlay.js
+++ js/overlay.js
@@ -1,9 +1,3 @@
-// function on() {
-//   document.getElementById("overlay").style.display = "block";
-// }
-// function off() {
-//   document.getElementById("overlay").style.display = "none";
-// }
 
 // doughnut chart
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* scroll */
+
 $(document).ready(function() {
   // scroll-top arrow only appears after scrolling down half the page
   $(window).scroll(function() {
@@ -9,7 +11,6 @@ $(document).ready(function() {
   });
 });
 
-// knappar i overlay
 
 // const overlayTrigger = document.querySelector('.thumbnail');
 // const overlayTrigger = document.getElementById("md-button");
@@ -35,6 +36,11 @@ $(document).ready(function() {
 // function off() {
 //   document.getElementById("overlay").style.display = "none";
 // }
+
+
+
+/* overlay funktion plusOne, minusOne och totalPrice */
+
 function plusOne(x = document.getElementById("hour").value) {
   x = ++x;
   if (x >= 1) {
@@ -62,7 +68,8 @@ function totalPrice(x = document.getElementById("hour").value) {
   document.getElementById("tprice").value = tprice;
 }
 
-// ovarlay chart
+
+// overlay chart
 // var myCanvas = document.getElementById("myCanvas");
 // myCanvas.width = 180;
 // myCanvas.height = 180;
@@ -76,7 +83,6 @@ function totalPrice(x = document.getElementById("hour").value) {
   // ctx.stroke();
 // }
 
-<<<<<<< HEAD
 // function drawArc(ctx, centerX, centerY, radius, startAngle, endAngle) {
 //   ctx.beginPath();
 //   ctx.arc(centerX, centerY, radius, startAngle, endAngle);
@@ -200,80 +206,3 @@ function totalPrice(x = document.getElementById("hour").value) {
 
 // thumbnails.addEventListener('click', openOverlay);
 // overlayClose.addEventListener('click', closeOverlay);
-=======
-drawLine(ctx, 50, 50, 50, 50);
-drawArc(ctx, 100, 100, 100, 0, Math.PI / 3);
-drawPieSlice(
-    ctx,
-    100,
-    100,
-    100,
-    Math.PI / 2,
-    Math.PI / 2 + Math.PI / 4,
-    "#269bbc"
-);
-
-var myFeatures = {
-    Strengts: 35,
-    Skill: 25,
-    Brains: 9,
-    Gadget: 11,
-    Heart: 20
-};
-
-var Piechart = function (options) {
-    this.options = options;
-    this.canvas = options.canvas;
-    this.ctx = this.canvas.getContext("2d");
-    this.colors = options.colors;
-
-    this.draw = function () {
-        var total_value = 0;
-        var color_index = 0;
-        for (var categ in this.options.data) {
-            var val = this.options.data[categ];
-            total_value += val;
-        }
-
-        var start_angle = 0;
-        for (categ in this.options.data) {
-            val = this.options.data[categ];
-            var slice_angle = (2 * Math.PI * val) / total_value;
-
-            drawPieSlice(
-                this.ctx,
-                this.canvas.width / 2,
-                this.canvas.height / 2,
-                Math.min(this.canvas.width / 2, this.canvas.height / 2),
-                start_angle,
-                start_angle + slice_angle,
-                this.colors[color_index % this.colors.length]
-            );
-
-            start_angle += slice_angle;
-            color_index++;
-        }
-
-        if (this.options.doughnutHoleSize) {
-            drawPieSlice(
-                this.ctx,
-                this.canvas.width / 2,
-                this.canvas.height / 2,
-                this.options.doughnutHoleSize *
-                Math.min(this.canvas.width / 2, this.canvas.height / 2),
-                0,
-                2 * Math.PI,
-                "#269bbc"
-            );
-        }
-    };
-};
-
-var myDougnutChart = new Piechart({
-    canvas: myCanvas,
-    data: myFeatures,
-    colors: ["#3ED1EE", "#BCD689", "#824583", "#FFD458", "#FF9241"],
-    doughnutHoleSize: 0.5
-});
-myDougnutChart.draw();
->>>>>>> 9d32cec410e854f9a15898077c5f88ea05ceb559
